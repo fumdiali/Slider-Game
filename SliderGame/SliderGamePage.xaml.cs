810c23e6f42d5787abe5aa07b48613a39dc54b75
Apple <apple@Apples-MacBook-Pro.local>
2018-04-28 12:57:47 +0100

    first commit

---
+++ SliderGame/SliderGamePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace SliderGame
+{
+    public partial class SliderGamePage : ContentPage
+    {
+        public SliderGamePage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChange(object sender, ValueChangedEventArgs args){
+            double value = slider.Value;
+            if (value > 30)
+            {
+                DisplayAlert("Good", "Your score is over 30", "OK");
+            }
+            else
+            {
+                playerScore.Text = string.Format("Score {0}", value);
+            }
+        }
+
+        void ResetButton(object sender, EventArgs e){
+            playerScore.Text = "";
+        }
+
+      
+    }
+}
